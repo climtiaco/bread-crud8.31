50b038f53c1e01bb06a7dab6520d9c18b6b36895
Christian Limtiaco <climtiaco.code@gmail.com>
2023-09-12 23:51:59 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,16 +3,25 @@ const breadRouter = express.Router()
 const breadData = require('../models/bread.js') 
 
 //INDEX
+// So when i put res.render and the view i'm attaching is with the 'index.jsx' file, everything in this object that I'm putting in, will be input to that jsx file. 
 breadRouter.get('/', (req, res) => {
     res.render('index',
     {
-        breads: breadData
+        breads: breadData,
+        title: 'Index Page'
     })
 })
 
 //SHOW
+//Here I'm changing res.send(breadData[req.params.arrayIndex]) now to res.render('Show', {}) so that it refers to the show.jsx page and then I can add that second argument as an object that reads exactly what I want it to 
 breadRouter.get('/:arrayIndex', (req, res) => {
-    res.send(breadData[req.params.arrayIndex])
+    if (breadData[req.params.arrayIndex]) {
+    res.render('Show', {
+        bread: breadData[req.params.arrayIndex]
+    })
+    } else {
+        res.send('404')
+    }
 })
 
 
--- views/index.jsx
+++ views/index.jsx
@@ -1,16 +1,22 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index({breads}) {
+
+//So the "breads" defined in the argument of the function is pulling from the breads_controller.js file. That variable connects the breadData
+// Then how we're adding data to the page is we're creating html elements to incase all of our data that we're pulling from the other files.
+function Index({breads, title}) {
     return (
-        <Default>
+        <Default title={title}>
             <h2>Index Page</h2>
             <ul>
                 {
                     breads.map((bread, index) => {
-                        return (<li key={bread.id}>
-                            <a href={`/breads/${index}`}>{bread.name}</a>
+                        return (<li key={index}>
+                            <a href={`/breads/${index}`}>
+                                {bread.name}
+                                </a>
                             </li>)
+                            
                     })
                 }
             </ul>
